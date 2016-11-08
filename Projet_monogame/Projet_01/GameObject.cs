9b9178982d02bd1eb27d5c32bcfddd8cbd3fa31c
YannickRoux <roux.yannick@carrefour.cegepvicto.ca>
2016-11-08 16:52:17 -0500

    premier envoie a github
    jeux monoGame

---
+++ Projet_monogame/Projet_01/GameObject.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_01
+{
+    class GameObject
+    {
+        public bool estVivant;
+        public Texture2D sprite;
+        public Vector2 position;
+        public Vector2 vitesse;
+
+    }
+}
